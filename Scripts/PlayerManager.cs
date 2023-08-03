42589448f1ef13bf2104a0981f60ab7bf8819c9d
Raviteja Rao Jupally <ravitejajupalli333@gmail.com>
2023-08-03 16:31:26 +0530

    Project Files

---
+++ Scripts/PlayerManager.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerManager : MonoBehaviour
+{
+
+    public float health = 100f;
+
+    public Text healthText;
+
+    public GameManager gameManager;
+
+
+    public void Hit(float damage)
+    {
+        health -= damage;
+        healthText.text =  health.ToString() + " Health";
+
+        if (health <= 0)
+        {
+            gameManager.EndGame();
+        }
+    }
+
+}
---
+++ Scripts/PlayerMovement.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public CharacterController Controller;
+    public float speed = 12f;
+    Vector3 velocity;
+    public float gravity = -9.81f;
+    public bool isGrounded;
+
+    public Transform groundCheck;
+    public float groundDistance = 0.4f;
+    public LayerMask groundMask;
+
+    public float jumpHeight = 2f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+        if(isGrounded && velocity.y < 0 ) 
+        {
+            velocity.y = -2f;
+        }
+
+
+        float X = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * X + transform.forward * z;
+        Controller.Move(move * speed * Time.deltaTime);
+
+        velocity.y += gravity * Time.deltaTime;
+
+        Controller.Move(velocity * Time.deltaTime);
+
+        if(Input.GetButtonDown("Jump") && isGrounded)
+        {
+
+            velocity.y = Mathf.Sqrt(jumpHeight * -2 * gravity);
+        }
+
+    }
+}
